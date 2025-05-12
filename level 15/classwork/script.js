07072e5b53111ec13890d1222bffb75e473aef62
nikolozlomidze19 <nicklomidze111@gmail.com>
2025-05-13 01:28:57 +0400

    second commit

---
+++ level 15/classwork/script.js
@@ -0,0 +1,34 @@
+const question = [
+    {
+        question: "which is the largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue whale", correct: true },     
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false },
+        ]
+    },
+    {
+        question: "which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false },
+            { text: "Australia", correct: true },     
+            { text: "Arctic", correct: false },
+            { text: "Africa", correct: false },
+        ]
+     }  
+    ]
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
