f29f65acaed634f281f3ea3f43e8560849b9b08c
nikolozlomidze19 <nicklomidze111@gmail.com>
2025-06-05 17:58:00 +0400

    second commit

---
+++ level 16/homework/homework1.py
@@ -0,0 +1,7 @@
+num=int(input("enter your fav number: "))
+if num %2==0:
+    print(num, "ლუწია")
+
+
+else:
+    print(num, "კენტია")    
---
+++ level 16/homework/homework2.py
@@ -0,0 +1,7 @@
+name=input("enter your name:")
+if name=="nick":
+    print("hello")
+
+else:
+    print("bye")
+
